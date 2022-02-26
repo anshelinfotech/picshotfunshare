b4d2ca13fe6e2a811b58fdf4f3cd4384374f9afd
Anshel Infotech <anshelinfotech@gmail.com>
2022-02-26 12:46:05 +0530

    HttpResponse Home

--- picshot/urls.py
+++ picshot/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.http import HttpResponse
 from django.contrib import admin
 from django.urls import path
 
+def home(request):
+    return HttpResponse('Welcome to PICSHOT !')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name=home),
+
 ]
